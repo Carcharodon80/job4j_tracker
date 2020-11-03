2f94262e3e24e6d1ad6b5b0acca9474ec84395c6
Carcharodon80 <karna241280@gmail.com>
2020-11-03 23:08:47 +0300

    1.1. Тесты на StartUI. [#340884]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -127,4 +127,24 @@ public class StartUITest {
                 "0. Find Items by Name" + ln +
                 "1. Exit" + ln));
     }
+
+    @Test
+    public void whenInvalidExi() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"99", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new Exit()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format("Menu%n"
+                        + "0. Exit%n"
+                        + "Wrong input, you can select: 0 .. 0%n"
+                        + "Menu%n"
+                        + "0. Exit%n"
+                )));
+    }
 }
