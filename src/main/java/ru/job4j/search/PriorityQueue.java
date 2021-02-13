4c5add0723d7c8e762644bca3abef0abb2e61d8e
Carcharodon80 <karna241280@gmail.com>
2021-02-13 03:54:43 +0300

    temp

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -2,9 +2,21 @@ package ru.job4j.search;
 
 import java.util.LinkedList;
 
+/**
+ *
+ */
 public class PriorityQueue {
+    /**
+     *
+     */
     private LinkedList<Task> tasks = new LinkedList<>();
 
+    /**
+     * Метод принимает на вход заявку и добавляет ее в очередь.
+     * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
+     * они распределяются по принципу FIFO.
+     * @param task задача которая добавляется в очередь
+     */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
@@ -16,6 +28,10 @@ public class PriorityQueue {
         this.tasks.add(index, task);
     }
 
+    /**
+     *
+     * @return
+     */
     public Task take() {
         return tasks.poll();
     }
