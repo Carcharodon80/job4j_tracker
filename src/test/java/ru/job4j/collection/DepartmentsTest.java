8d7dd67b1416bd4e42f01deab16ff2c9c53d49c4
Carcharodon80 <karna241280@gmail.com>
2021-04-18 00:57:21 +0300

    1. Метод .of [#340687]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -16,7 +16,7 @@ public class DepartmentsTest {
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1", "k2/sk1/ssk2");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk2");
+        List<String> expect = List.of("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/ssk2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -24,7 +24,7 @@ public class DepartmentsTest {
     @Test
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
@@ -33,7 +33,7 @@ public class DepartmentsTest {
     public void sortAsc() {
         List<String> input = Arrays.asList("k1/sk1", "k1/sk2", "k1/sk1/ssk1", "k1/sk1/ssk2",
                 "k2", "k2/sk1/ssk1", "k2/sk1/ssk2");
-        List<String> expected = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2",
+        List<String> expected = List.of("k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2",
                 "k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2");
         List<String> result = Departments.fillGaps(input);
         result.sort(Comparator.naturalOrder());
@@ -44,7 +44,7 @@ public class DepartmentsTest {
     public void sortDesc() {
         List<String> input = Arrays.asList("k1/sk1", "k1/sk2", "k1/sk1/ssk1", "k1/sk1/ssk2",
                 "k2", "k2/sk1/ssk1", "k2/sk1/ssk2");
-        List<String> expected = Arrays.asList("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2",
+        List<String> expected = List.of("k2", "k2/sk1", "k2/sk1/ssk1", "k2/sk1/ssk2",
                 "k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2", "k1/sk2");
         List<String> result = Departments.fillGaps(input);
         result.sort(new DepDescComp());
