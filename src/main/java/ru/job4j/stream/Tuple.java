4cf254dee07cdf8241cff4eddc9eb8fd29937da5
Carcharodon80 <karna241280@gmail.com>
2021-04-25 00:39:25 +0300

    2. Аттестация [#340693]

---
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -0,0 +1,38 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+/**
+ * 2. Аттестация [#340693]
+ */
+public class Tuple {
+    private String name;
+    private double score;
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Tuple tuple = (Tuple) o;
+        return Double.compare(tuple.score, this.score) == 0 &&
+                Objects.equals(this.name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+}
