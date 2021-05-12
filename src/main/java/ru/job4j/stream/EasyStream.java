a00933b4655f72e0f568a1de2eaa0039156c5863
Carcharodon80 <karna241280@gmail.com>
2021-05-13 00:25:07 +0300

    EasyStream [#401629]

---
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -0,0 +1,43 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * EasyStream [#401629]
+ */
+public class EasyStream {
+    List<Integer> integerList;
+
+    public EasyStream(List<Integer> integerList) {
+        this.integerList = integerList;
+    }
+
+    public static EasyStream of(List<Integer> source) {
+        return new EasyStream(source);
+    }
+
+    public EasyStream map(Function<Integer, Integer> fun) {
+        List<Integer> dest = new ArrayList<>();
+        for (Integer integer : integerList) {
+            dest.add(fun.apply(integer));
+        }
+        return new EasyStream(dest);
+    }
+
+    public EasyStream filter(Predicate<Integer> fun) {
+        List<Integer> dest = new ArrayList<>();
+        for (Integer integer : integerList) {
+            if (fun.test(integer)) {
+                dest.add(integer);
+            }
+        }
+        return new EasyStream(dest);
+    }
+
+    public List<Integer> collect() {
+        return integerList;
+    }
+}
